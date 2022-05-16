7874a4eab33b0cb888dcd06c5d96011ebc1b8bf1
FranSabt <hsabatino1@gmail.com>
2022-05-16 16:11:48 -0400

    I make to work the data from the scroll input, now a need to fix the grid

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 
 const gridVal = document.getElementById("grid-control");
 const output = document.getElementById("grid-size");
+
 output.innerHTML = gridVal.value;
+
+
+
 gridVal.oninput = function() {
     output.innerHTML = this.value;
+    let size = gridVal.value; //Works! It give us a value to work
+    console.log(size)
+    document.getElementById("grid").style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    document.getElementById("grid").style.gridTemplateRows = `repeat(${size}, 1fr)`
+    const element = document.createElement("div");
+    for(let i = 0; i < size; i++){
+        const newDiv = document.getElementById("grid");
+        newDiv.appendChild(element).className = "box"
+        //newDiv
+        
+    }
+    size = 0;
 }
 
 
-console.log(gridVal)
