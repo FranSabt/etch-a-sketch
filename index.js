36540d737792ecab6ef2da2c226bf1f17f61e725
FranSabt <88568637+FranSabt@users.noreply.github.com>
2022-05-24 21:28:07 -0400

    i don't like this solution

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ window.onload = () =>{
 
     let defaultSize = 16;
     let size;
+    let boxes
 
     const gridVal = document.getElementById("grid-control");
     const output = document.getElementById("grid-size");
 
     output.innerHTML = gridVal.value;
-
-
-    if(!output.val){
-        size = defaultSize
-    }
 
     gridVal.oninput = function() {
         output.innerHTML = this.value;
@@ -31,8 +27,17 @@ window.onload = () =>{
             const newDiv = document.getElementById("grid");
             newDiv.appendChild(element).className = "box"
             newDiv.appendChild(element).attributes = 'onclick="colorR()"'
-            
-            
+        }
+        boxes = document.getElementsByClassName('box'); //quizas un while solucione esto
+        console.log(boxes); // 👉️ [div.box, div.box, div.box]
+        
+        for (const box of boxes) {
+            box.addEventListener('click', function onClick() {
+                color = rbColor();
+                let hexStr = color.toString(16);
+                console.log(hexStr);
+                box.style.backgroundColor = `#${hexStr}`
+            });
         }
     }
 
@@ -66,18 +71,20 @@ window.onload = () =>{
 
     // solo se está ejecutando en el default
     const rbColor = () => {
-        return Math.floor(Math.random() * 16777217 + 1);
+        return Math.floor(Math.random() * 16777216);
     }
 
-    const boxes = document.getElementsByClassName('box'); //quizas un while solucione esto
+    boxes = document.getElementsByClassName('box'); //quizas un while solucione esto
     console.log(boxes); // 👉️ [div.box, div.box, div.box]
 
     // ✅ addEventListener to all boxes
     for (const box of boxes) {
-    box.addEventListener('click', function onClick() {
-        color = '#' + rbColor();
-        console.log(color)
-        box.style.backgroundColor = `${color}`
-    });
+        box.addEventListener('click', function onClick() {
+            color = rbColor();
+            let hexStr = color.toString(16);
+            console.log(hexStr);
+            box.style.backgroundColor = `#${hexStr}`
+        });
     }
+    
 }
